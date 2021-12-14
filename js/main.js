649e7e886b5bd423b25d5f61bb391d99ba9e29d7
AssiaSv <svornikova2013@yandex.ru>
2021-12-14 16:22:23 +0300

    create: adaptive for modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 var burger = document.querySelector(".burger");
 burger.addEventListener("click", function () {
-  console.log("клик по кнопке меню");
+  // console.log("клик по кнопке меню");
   document.querySelector(".navbar-right").classList.toggle("navbar-right--visible");
 });
 
